46f6ec66f323cf741c661de13ca8f166c3fae617
Oleksandr_Velychko2 <Alex.Velichko@gmail.com>
2022-05-12 15:25:46 +0300

    Update auth mode

--- src/main/java/com/ovelychko/WebSecurityConfiguration.java
+++ src/main/java/com/ovelychko/WebSecurityConfiguration.java
@@ -1,39 +1,22 @@
 package com.ovelychko;
 
-//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableOAuth2Sso
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
-                .antMatcher("/**")
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/error")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
-
-//        http
-//                .csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/", "/index", "/error").permitAll() // Allow this for all
-//                .anyRequest().authenticated()
-//                .and().logout().logoutSuccessUrl("/").permitAll()
-//                .and()
-//                .oauth2Login();
-
-//        http.authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .oauth2Login();
+                .antMatchers("/", "/index", "/error").permitAll() // Allow this for all
+                .anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and()
+                .oauth2Login();
     }
 }
